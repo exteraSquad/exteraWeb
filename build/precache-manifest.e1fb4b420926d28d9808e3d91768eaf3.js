7b87ff269fa07ddb79141de9c0b5adc910eada57
kirillsaint <kirillsaint@wtfuck.tk>
2022-05-08 20:33:38 +0600

    first

---
+++ build/precache-manifest.e1fb4b420926d28d9808e3d91768eaf3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2b428b6ae4277901b9848a8861b8f4",
+    "url": "/exteraweb-build/index.html"
+  },
+  {
+    "revision": "0f7aca7000a09d3f71d3",
+    "url": "/exteraweb-build/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "92f4a32c6dfb8e1d82f7",
+    "url": "/exteraweb-build/static/css/4.b932f582.chunk.css"
+  },
+  {
+    "revision": "2effd3643f0d5db8f9ab",
+    "url": "/exteraweb-build/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "bc656b5b44010e1b708a",
+    "url": "/exteraweb-build/static/css/7.53b0317a.chunk.css"
+  },
+  {
+    "revision": "8f1bd0709278cdbfb727",
+    "url": "/exteraweb-build/static/css/main.2d7149bd.chunk.css"
+  },
+  {
+    "revision": "0f7aca7000a09d3f71d3",
+    "url": "/exteraweb-build/static/js/0.f4f676ce.chunk.js"
+  },
+  {
+    "revision": "966d363519c681b2772a",
+    "url": "/exteraweb-build/static/js/3.d1ab0a00.chunk.js"
+  },
+  {
+    "revision": "92f4a32c6dfb8e1d82f7",
+    "url": "/exteraweb-build/static/js/4.0bbaba61.chunk.js"
+  },
+  {
+    "revision": "2effd3643f0d5db8f9ab",
+    "url": "/exteraweb-build/static/js/5.6da872d7.chunk.js"
+  },
+  {
+    "revision": "da37687f0ce04b4ec232",
+    "url": "/exteraweb-build/static/js/6.1aacc081.chunk.js"
+  },
+  {
+    "revision": "bc656b5b44010e1b708a",
+    "url": "/exteraweb-build/static/js/7.b359cbd8.chunk.js"
+  },
+  {
+    "revision": "8f1bd0709278cdbfb727",
+    "url": "/exteraweb-build/static/js/main.8be40512.chunk.js"
+  },
+  {
+    "revision": "88fdcfef6eae09eed6c6",
+    "url": "/exteraweb-build/static/js/runtime~main.17c4abfc.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/exteraweb-build/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/exteraweb-build/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/exteraweb-build/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "72c6a620aa333a157798f665ffdd87e0",
+    "url": "/exteraweb-build/static/media/telegram-logo.72c6a620.svg"
+  }
+]);
